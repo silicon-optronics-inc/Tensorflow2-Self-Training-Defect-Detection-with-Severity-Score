50a5bc89d11a53a72cc8446600f42804ceb127c5
Po-Kai Chen <34348942+pkchen53@users.noreply.github.com>
2020-09-25 15:24:36 +0800

    Add files via upload

---
+++ object_detection/train/run.bat
@@ -0,0 +1,13 @@
+call C:/ProgramData/Anaconda3/Scripts/activate.bat
+SET wd=%~dp0
+D:
+cd %wd%
+
+
+REM 0: train only, 1: train + evaluate, 2: semi-supervised learning, 3: evaluate only
+SET mode=2
+SET pipeline_config=faster_rcnn_shallownet_dashcol.config
+
+SET tool_path=%~dp0/tool
+python %tool_path%/control_main.py --mode=%mode% --work_directory=%wd% --pipeline_config=%pipeline_config%
+pause
